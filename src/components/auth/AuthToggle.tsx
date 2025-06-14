8595d174484638ebaccc55ede333e9b32fa53a64
ahmed-elhagar <86900642+ahmed-elhagar@users.noreply.github.com>
2025-06-14 22:49:13 +0300

    remove alot of files

--- src/components/auth/AuthToggle.tsx
+++
@@ -1,4 +0,0 @@
-
-// Re-export from features structure
-export { AuthToggle } from '../../features/auth/components/AuthToggle';
-export { default } from '../../features/auth/components/AuthToggle';
--- src/components/exercise/ExerciseListEnhanced.tsx
+++
@@ -1,4 +0,0 @@
-
-// Re-export from features structure
-export { ExerciseListEnhanced as default } from '../../features/exercise/components/ExerciseListEnhanced';
-export { ExerciseListEnhanced } from '../../features/exercise/components/ExerciseListEnhanced';
